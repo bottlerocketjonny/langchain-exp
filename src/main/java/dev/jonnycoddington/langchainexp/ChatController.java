f34a5091e724ae88cc78e92234285bfbdad96138
Jonny Coddington <jonny@fintek.io>
2023-11-26 16:56:49 +0000

    Docs updated

--- src/main/java/dev/jonnycoddington/langchainexp/ChatController.java
+++ src/main/java/dev/jonnycoddington/langchainexp/ChatController.java
@@ -17,6 +17,14 @@ public class ChatController {
     this.chatService = chatService;
   }
   
+  /**
+   * Chat with an LLM.
+   *
+   * @param systemMessage The system message with a description of how you want the LLM to act
+   * @param userMessage The user message containing the question
+   * @param modelId The id of the HuggingFace model to use. If null, this will default to OpenAI's 'gpt-3.5-turbo' model.
+   * @return The response from the LLM
+   */
   @PostMapping("/chat-with-model")
   public String chatWithModel(@RequestParam String systemMessage,
       @RequestParam String userMessage,
@@ -24,6 +32,15 @@ public class ChatController {
     return chatService.chatWithModel(systemMessage, userMessage, modelId);
   }
   
+  /**
+   * Interact with an LLM using your provided document as context via an embedding.
+   *
+   * @param question The question to ask the LLM
+   * @param fileName The name of the file you'd like to use as an embedding
+   * @param modelId The id of the HuggingFace model to use. If null, this will default to the OpenAI 'gpt-3.5-turbo' model
+   * @param maxResults The maximum number of text segments retrieved from the embedding store
+   * @return The response from the LLM
+   */
   @PostMapping("/chat-with-docs")
   public String chatWithDocs(@RequestParam String question,
       @RequestParam String fileName,
